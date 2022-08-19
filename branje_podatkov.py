914f649c5f003e38fc9fee033626a22f38f25840
MightyOwler <jasa.knap@gmail.com>
2022-08-19 23:01:45 +0200

    Shranjeni preprosti podatki v beležko

--- branje_podatkov.py
+++ branje_podatkov.py
@@ -24,11 +24,12 @@ class Meritev():
         self.besedilo_po_elementih = besedilo_meritve.split(", ")
     
     def najdi_element(self, ime_elementa):
-        element = None
+        element = []
         for i in self.besedilo_po_elementih:
             if ime_elementa in i:
-                element = i[len(ime_elementa) + 1:]
-                break
+                element.append(i[len(ime_elementa) + 1:])
+                #tole bo zraven napisalo še ime
+                #element.append(i)
         
         if element:
             return element
@@ -74,14 +75,48 @@ class Meritev():
     def najdi_meja_dU(self):
         return self.najdi_element('Meja(dU):')
     
+    # to ni popolno, saj ne odstrani Page
+    def najdi_pot(self):
+        return ["P" + self.najdi_element('Pot:')[0]]
+    
     
 mnozica_vseh_objektov_meritev = [Meritev(i) for i in loceno_besedilo_discardane_prazne]   
 #print(mnozica_vseh_objektov_meritev[10].besedilo_po_elementih)
 
 
+with open("poenostavljeni_podatki.txt", "w", encoding="utf-8") as f:
+    for j,i in enumerate(mnozica_vseh_objektov_meritev):
+    #print(i.besedilo_po_elementih)
+        f.write("Številka meritve"+ " " + str(j + 1))
+        f.write("\n")
+        f.write(str(i.najdi_tip_varovalke()))
+        f.write("\n")
+        f.write(str(i.najdi_I_varovalke()))
+        f.write("\n")
+        f.write(str(i.najdi_t_varovalke()))
+        f.write("\n")
+        f.write(str(i.najdi_Isc_faktor()))
+        f.write("\n")
+        f.write(str(i.najdi_dU()))
+        f.write("\n")
+        f.write(str(i.najdi_Z_LPE()))
+        f.write("\n")
+        f.write(str(i.najdi_I_preizkusa()))
+        f.write("\n")
+        f.write(str(i.najdi_Ipsc_LPE()))
+        f.write("\n")
+        f.write(str(i.najdi_Uln()))
+        f.write("\n")
+        f.write(str(i.najdi_R()))
+        f.write("\n")
+        f.write(str(i.najdi_Zref()))
+        f.write("\n")
+        f.write(str(i.najdi_meja_dU()))
+        f.write("\n")
+        f.write(str(i.najdi_pot()))
+        f.write("\n----------------------------------------------------------------------------------------------------\n")
 
-
-for j,i in enumerate(mnozica_vseh_objektov_meritev[:100]):
+for j,i in enumerate(mnozica_vseh_objektov_meritev):
     #print(i.besedilo_po_elementih)
     print("Številka meritve", j+1)
     print(i.najdi_tip_varovalke())
@@ -96,10 +131,11 @@ for j,i in enumerate(mnozica_vseh_objektov_meritev[:100]):
     print(i.najdi_R())
     print(i.najdi_Zref())
     print(i.najdi_meja_dU())
+    print(i.najdi_pot())
     print("------------------")
 
     
 
-najin_objekt = mnozica_vseh_objektov_meritev[92]
+najin_objekt = mnozica_vseh_objektov_meritev[85]
 print(najin_objekt.besedilo)
 print(najin_objekt.besedilo_po_elementih)
