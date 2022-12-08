c86b6791e4642b65b7782a81572b4ce4152ff564
MightyOwler <jasa.knap@gmail.com>
2022-12-08 08:54:19 +0100

    Refactoring meje.py

--- meje.py
+++ meje.py
@@ -1,5 +1,15 @@
 from openpyxl import load_workbook
 
+rdeca = "FF0000"
+oranzna = "FFA500"
+modra = "99FFFF"
+
+def preberi_float_z_mesta_v_seznamu(seznam, mesto, pomozno_mesto = None):
+    if pomozno_mesto:
+        return float(seznam[mesto].split("/")[pomozno_mesto].replace(",",".")) if seznam[mesto].split("/")[pomozno_mesto] != "X" else "X"
+    else:
+        return float(seznam[mesto].replace(",",".")) if seznam[mesto] != "X" else "X"
+
 def preveri_meje_osnovne(seznam, trafo = True):
     # funkcija prejme seznam s 25 elementi, ter binarno vrendost trafo
     
@@ -10,53 +20,47 @@ def preveri_meje_osnovne(seznam, trafo = True):
     faktor_za_oranzno_barvo_upornost = 0.7    #0.7 pomeni, da je barva oranžna v primeru, ko je rezultat večji od 0.7-kratnika najvišje dovoljene meje
     
     tip_varovalke = seznam[8]
-    i_varovalke = float(seznam[9].replace(",",".")) if seznam[9] != "X" else "X"
-    t_varovalke = float(seznam[10].replace(",",".")) if seznam[10] != "X" else "X"
+    i_varovalke = preberi_float_z_mesta_v_seznamu(seznam, 9)
+    t_varovalke = preberi_float_z_mesta_v_seznamu(seznam, 10)
     
     pot = seznam[25]
-    if "lamp" in pot or "Lamp" in pot:
-        # luc = True
-        if trafo:
-            meja_du = 0.05
-        else:
-            meja_du = 0.03
-    else:
-        if trafo:
-            meja_du = 0.07
-        else:
-            meja_du = 0.05
-        
-
-    # tale koda je problematična (zaenkrat ne več, ker sva popravila)
     
-    ik1 = float(seznam[11].split("/")[1].replace(",",".")) if seznam[11].split("/")[1] != "X" else "X"
-    ik2 = float(seznam[12].split("/")[1].replace(",",".")) if seznam[12].split("/")[1] != "X" else "X"
-    zs = float(seznam[11].split("/")[0].replace(",",".")) if seznam[11].split("/")[0] != "X" else "X"
-    zl = float(seznam[12].split("/")[0].replace(",",".")) if seznam[12].split("/")[0] != "X" else "X"
-    du = float(seznam[12].split("/")[2].replace(",",".")) / 100 if seznam[12].split("/")[2] != "X" else "X" 
+    # Če je luč, obravnavamo mejo drugače
+    if "lamp" in pot or "Lamp" in pot:
+        meja_du = 0.05 if trafo else 0.03
+    else:
+        meja_du = 0.07 if trafo else 0.05
+
+    ik1 = preberi_float_z_mesta_v_seznamu(seznam, 11, pomozno_mesto=1)
+    ik2 = preberi_float_z_mesta_v_seznamu(seznam, 12, pomozno_mesto=1)
+    zs = preberi_float_z_mesta_v_seznamu(seznam, 11, pomozno_mesto=0)
+    zl = preberi_float_z_mesta_v_seznamu(seznam, 11, pomozno_mesto= 0)
+    du = preberi_float_z_mesta_v_seznamu(seznam, 12, pomozno_mesto= 2)
+    if type(du) == float:
+        du /= 100
     
     rlow = seznam[5]
     riso = seznam[7]
     
     if rlow != "X":
         if ">" in rlow:
-            slovar_problematicnih_meritev[5] = "FF0000"
+            slovar_problematicnih_meritev[5] = rdeca
         else:
             # meja_rlow = 1
             rlow = float(rlow.replace(",","."))
             if rlow > 1:
-                slovar_problematicnih_meritev[5] = "FF0000"
+                slovar_problematicnih_meritev[5] = rdeca
             elif rlow > 0.8: 
-                slovar_problematicnih_meritev[5] = "FFA500"
+                slovar_problematicnih_meritev[5] = oranzna
             
     elif riso != "X":
         if not ">" in riso:
             # meja_riso = 1
             riso = float(riso.replace(",","."))
             if riso < 1:
-                slovar_problematicnih_meritev[7] = "FF0000"
+                slovar_problematicnih_meritev[7] = rdeca
             elif riso < 2: 
-                slovar_problematicnih_meritev[7] = "FFA500"
+                slovar_problematicnih_meritev[7] = oranzna
 
     excel_delovna_datoteka = load_workbook("Meje za meritve.xlsx", data_only=True)
 
@@ -69,7 +73,7 @@ def preveri_meje_osnovne(seznam, trafo = True):
             break
     
     if not t_varovalke_je_ustrezen:
-        slovar_problematicnih_meritev[10] = "FF0000"
+        slovar_problematicnih_meritev[10] = rdeca
         stolpec = 1
 
     if tip_varovalke in ["gG", "NV", "gL"]:
@@ -77,45 +81,31 @@ def preveri_meje_osnovne(seznam, trafo = True):
         prva_vrstica = 6
         zadnja_vrstica = 34
         
+        slovar_t_varovalk_in_stolpcev = {0.1: 1, 0.2: 4, 0.4:7, 5.0:10}
+        
         if t_varovalke == 0.1:
-            stolpec = 1
             zadnja_vrstica = 30
-        elif t_varovalke == 0.2:
-            stolpec = 4
-        elif t_varovalke == 0.4:
-            stolpec = 7 
-        elif t_varovalke == 5.0:
-            stolpec = 10
-
-                        
+        
+        stolpec = slovar_problematicnih_meritev.get(t_varovalke, 0)
+        
         stolpec1 = [excel_delovni_list.cell(row=i,column=stolpec).value for i in range(prva_vrstica, zadnja_vrstica + 1)]
         stolpec2 = [excel_delovni_list.cell(row=i,column=stolpec + 1).value for i in range(prva_vrstica, zadnja_vrstica + 1)]
         stolpec3 = [excel_delovni_list.cell(row=i,column=stolpec + 2).value for i in range(prva_vrstica, zadnja_vrstica + 1)]
         
         if i_varovalke not in stolpec1:
             indeks_ujemajoce_varovalke = 0
-            slovar_problematicnih_meritev[9] = "FF0000"
+            slovar_problematicnih_meritev[9] = rdeca
         else:
             indeks_ujemajoce_varovalke = stolpec1.index(i_varovalke)            
         
     if tip_varovalke in ["B","D","C"]:
-        pass
-    
-        """
-        Tukaj morava dodati meje ta meritve oblike C
-        """
-        
         excel_delovni_list = excel_delovna_datoteka["BCD"]
         
         prva_vrstica = 6
         zadnja_vrstica = 17
         
-        if tip_varovalke == "B":
-            stolpec = 2
-        if tip_varovalke == "C":
-            stolpec = 4
-        if tip_varovalke == "D":
-            stolpec = 6
+        slovar_tipov_varovalk_in_stolpcev = {"B": 2, "C": 4, "D": 6}
+        stolpec = slovar_tipov_varovalk_in_stolpcev[tip_varovalke]
 
         stolpec1 = [excel_delovni_list.cell(row=i,column=1).value for i in range(prva_vrstica, zadnja_vrstica + 1)]
         stolpec2 = [excel_delovni_list.cell(row=i,column=stolpec).value for i in range(prva_vrstica, zadnja_vrstica + 1)]
@@ -123,50 +113,50 @@ def preveri_meje_osnovne(seznam, trafo = True):
         
         if i_varovalke not in stolpec1:
             indeks_ujemajoce_varovalke = 0
-            slovar_problematicnih_meritev[9] = "FF0000"
+            slovar_problematicnih_meritev[9] = rdeca
         else:
             indeks_ujemajoce_varovalke = stolpec1.index(i_varovalke)
         
     if ik1 == "X":
-        slovar_problematicnih_meritev[11] = "99FFFF"
+        slovar_problematicnih_meritev[11] = modra
     elif ik2 == "X":
-        slovar_problematicnih_meritev[12] = "99FFFF"
+        slovar_problematicnih_meritev[12] = modra
     elif t_varovalke_je_ustrezen:
         if ik1 < stolpec2[indeks_ujemajoce_varovalke]: 
-            slovar_problematicnih_meritev[11] = "FF0000"
+            slovar_problematicnih_meritev[11] = rdeca
         elif ik1 <= stolpec2[indeks_ujemajoce_varovalke] * faktor_za_oranzno_barvo_tok: 
-            slovar_problematicnih_meritev[11] = "FFA500"
+            slovar_problematicnih_meritev[11] = oranzna
         
         if ik2 < stolpec2[indeks_ujemajoce_varovalke]:
-            slovar_problematicnih_meritev[12] = "FF0000"
+            slovar_problematicnih_meritev[12] = rdeca
         elif ik2 == stolpec2[indeks_ujemajoce_varovalke] * faktor_za_oranzno_barvo_tok:
-            slovar_problematicnih_meritev[12] = "FFA500"
+            slovar_problematicnih_meritev[12] = oranzna
     
     if zs == "X":
-        slovar_problematicnih_meritev[11] = "99FFFF"
+        slovar_problematicnih_meritev[11] = modra
     elif zl == "X":
-        slovar_problematicnih_meritev[12] = "99FFFF"
+        slovar_problematicnih_meritev[12] = modra
     elif du == "X":
-        slovar_problematicnih_meritev[12] = "99FFFF"
+        slovar_problematicnih_meritev[12] = modra
     elif t_varovalke_je_ustrezen:
         if zl > stolpec3[indeks_ujemajoce_varovalke]:
-            slovar_problematicnih_meritev[12] = "FF0000"
+            slovar_problematicnih_meritev[12] = rdeca
         elif zl >= stolpec3[indeks_ujemajoce_varovalke] * faktor_za_oranzno_barvo_upornost:
-            if slovar_problematicnih_meritev.get(12, "") != "FF0000":
-                slovar_problematicnih_meritev[12] = "FFA500"   
+            if slovar_problematicnih_meritev.get(12, "") != rdeca:
+                slovar_problematicnih_meritev[12] = oranzna   
             
             
         if zs > stolpec3[indeks_ujemajoce_varovalke]:
-            slovar_problematicnih_meritev[11] = "FF0000"
+            slovar_problematicnih_meritev[11] = rdeca
         elif zs >= stolpec3[indeks_ujemajoce_varovalke] * faktor_za_oranzno_barvo_upornost:
-            if slovar_problematicnih_meritev.get(11, "") != "FF0000":
-                slovar_problematicnih_meritev[11] = "FFA500"
+            if slovar_problematicnih_meritev.get(11, "") != rdeca:
+                slovar_problematicnih_meritev[11] = oranzna
             
         if du > meja_du:
-            slovar_problematicnih_meritev[12] = "FF0000"
+            slovar_problematicnih_meritev[12] = rdeca
         elif du >= meja_du * faktor_za_oranzno_barvo_upornost:
-            if slovar_problematicnih_meritev.get(12, "") != "FF0000":
-                slovar_problematicnih_meritev[12] = "FFA500"
+            if slovar_problematicnih_meritev.get(12, "") != rdeca:
+                slovar_problematicnih_meritev[12] = oranzna
     
     return slovar_problematicnih_meritev
 
@@ -177,16 +167,15 @@ def preveri_meje_RLOW4(seznam, trafo = True):
     
     if r != "":
         if ">" in r:
-            slovar_problematicnih_meritev[1] = "FF0000"
+            slovar_problematicnih_meritev[1] = rdeca
         else:
             r = float(r) if r != "X" else "X"
-            meja_rlow = 1
             
             if r == "X":
-                slovar_problematicnih_meritev[1] = "99FFFF"
+                slovar_problematicnih_meritev[1] = modra
             elif r > 1:
-                slovar_problematicnih_meritev[1] = "FF0000"
+                slovar_problematicnih_meritev[1] = rdeca
             elif r > 0.8: 
-                slovar_problematicnih_meritev[1] = "FFA500"
+                slovar_problematicnih_meritev[1] = oranzna
             
     return slovar_problematicnih_meritev
