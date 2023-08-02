47ecc97539f32d619eecdd2182991bb34bb800a8
MightyOwler <jasa.knap@gmail.com>
2023-08-02 20:56:43 +0200

    Odstranjenih nekaj odvečnih funkcij

--- meje_omare.py
+++ meje_omare.py
@@ -5,17 +5,6 @@ ORANZNA = "FFA500"
 MODRA = "99FFFF"
 
 
-def preberi_float_z_mesta_v_seznamu(seznam, mesto, pomozno_mesto=None):
-    if pomozno_mesto is not None:
-        return (
-            float(seznam[mesto].split("/")[pomozno_mesto].replace(",", "."))
-            if seznam[mesto].split("/")[pomozno_mesto] != "X"
-            else "X"
-        )
-    else:
-        return float(seznam[mesto].replace(",", ".")) if seznam[mesto] != "X" else "X"
-
-
 def preveri_meje_omare(seznam):
     # funkcija prejme seznam s 25 elementi, ter binarno vrendost trafo
 
--- meje_stroji.py
+++ meje_stroji.py
@@ -5,17 +5,6 @@ ORANZNA = "FFA500"
 MODRA = "99FFFF"
 
 
-def preberi_float_z_mesta_v_seznamu(seznam, mesto, pomozno_mesto=None):
-    if pomozno_mesto is not None:
-        return (
-            float(seznam[mesto].split("/")[pomozno_mesto].replace(",", "."))
-            if seznam[mesto].split("/")[pomozno_mesto] != "X"
-            else "X"
-        )
-    else:
-        return float(seznam[mesto].replace(",", ".")) if seznam[mesto] != "X" else "X"
-
-
 def preveri_meje_R_ISO(seznam, meja_izolacijske_upornosti_stroji_riso_oranzna=100):
     # funkcija prejme seznam s 25 elementi, ter binarno vrendost trafo
 
